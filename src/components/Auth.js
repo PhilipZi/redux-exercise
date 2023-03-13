0dcab47cb83ff7145b738cb7dc67ddc843aef9a6
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-13 19:19:56 +0100

    add userprofile-component add redux to userprofile component

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,11 @@
 import { Fragment } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import { authActions } from "../store";
 import classes from "./Auth.module.css";
 
 const Auth = () => {
   const dispatch = useDispatch();
-  const auth = useSelector((state) => state.auth.isAuthenticated);
 
   const loginHandler = () => {
     dispatch(authActions.login());
@@ -14,23 +13,21 @@ const Auth = () => {
 
   return (
     <Fragment>
-      {!auth && (
-        <main className={classes.auth}>
-          <section>
-            <form>
-              <div className={classes.control}>
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" />
-              </div>
-              <div className={classes.control}>
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
-              </div>
-              <button onClick={loginHandler}>Login</button>
-            </form>
-          </section>
-        </main>
-      )}
+      <main className={classes.auth}>
+        <section>
+          <form>
+            <div className={classes.control}>
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <button onClick={loginHandler}>Login</button>
+          </form>
+        </section>
+      </main>
     </Fragment>
   );
 };

4c71c65e73ae904b6d8913ca386252b59696157a
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-13 19:13:19 +0100

    add redux to auth & header component add functions to auth & header

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,22 +1,37 @@
-import classes from './Auth.module.css';
+import { Fragment } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { authActions } from "../store";
+import classes from "./Auth.module.css";
 
 const Auth = () => {
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth.isAuthenticated);
+
+  const loginHandler = () => {
+    dispatch(authActions.login());
+  };
+
   return (
-    <main className={classes.auth}>
-      <section>
-        <form>
-          <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
-          </div>
-          <button>Login</button>
-        </form>
-      </section>
-    </main>
+    <Fragment>
+      {!auth && (
+        <main className={classes.auth}>
+          <section>
+            <form>
+              <div className={classes.control}>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" />
+              </div>
+              <div className={classes.control}>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" />
+              </div>
+              <button onClick={loginHandler}>Login</button>
+            </form>
+          </section>
+        </main>
+      )}
+    </Fragment>
   );
 };
 
